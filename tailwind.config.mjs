e6fc83a7b3469928fd55dcee0016e9572824f2ca
André Rodrigues <--get>
2024-12-10 20:09:38 +0000

    feat: color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors';
+import colors from "tailwindcss/colors";
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
@@ -14,21 +14,23 @@ export default {
       white: "#ffffff",
       gray: colors.gray,
       indigo: colors.indigo,
-      neutral: colors.neutral,  // Used mainly for text color
-      yellow: {
-        50: "#fefce8",
-        100: "#fef9c3",
-        400: "#facc15",
-        500: "#eab308",
+      neutral: colors.neutral, // Used mainly for text color
+      bostonBlue: {
+        50: "#f5f8f9",
+        100: "#e8f0f2",
+        200: "#d1e1e5",
+        300: "#b0ccd3",
+        400: "#7ca4b1",
+        500: "#3b8fab",
       }, // Accent colors, used mainly for star color, heading and buttons
-      orange: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fb713b",
-        400: "#fa5a15",
-        500: "#e14d0b",
-        600: "#ea580c",
+      blue: {
+        50: "#f5f7fa",
+        100: "#eaf0f5",
+        200: "#d5e2eb",
+        300: "#a3c1d4",
+        500: "#1f507a",
       }, // Primary colors, used mainly for links, buttons and svg icons
+
       red: colors.red, // Used for bookmark icon
       zinc: colors.zinc, // Used mainly for box-shadow
     },
