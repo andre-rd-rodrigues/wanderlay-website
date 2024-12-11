94e05d13562d91d71d6aff757c0c91a523dcb13e
André Rodrigues <--get>
2024-12-11 13:03:34 +0000

    feat: refactor services page

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,12 +1,14 @@
 import ogImageSrc from "@images/social.png";
 
 export const SITE = {
-  title: "ScrewFast",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  title: "As Marcas Mais Famosas",
+  tagline: "Materiais de Construção de Alta Qualidade",
+  description:
+    "As Marcas Mais Famosas oferece materiais de construção de primeira linha para atender às necessidades de todos os seus projetos. Explore nosso catálogo e entre em contato para garantir qualidade e confiabilidade em cada etapa da sua obra.",
+  description_short:
+    "Materiais de construção de alta qualidade para atender às necessidades do seu projeto.",
+  url: "https://asmarcasmaisfamosas.com.br",
+  author: "Wanderley de Carvalho",
 };
 
 export const SEO = {
@@ -34,6 +36,7 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  description:
+    "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
   image: ogImageSrc,
 };
